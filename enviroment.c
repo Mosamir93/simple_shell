760416a1c4f802b37edc3c708d92ba231f236a34
mohamed samir <mohamed_elshirbiny@hotmail.com>
2023-12-14 04:28:12 +0200

    13

--- enviroment.c
+++ enviroment.c
@@ -2,83 +2,85 @@
 
 /**
  * unsetenv_fun - unsets enviroment variable
- * @token: arrays of string from stdin
+ * @tokens: arrays of string from stdin
  * @name: name of the program
  * Return: -1 on success
-*/
+ */
 
 int unsetenv_fun(char **tokens, char *name)
 {
-    char **enviro;
-    char **new;
-    int i, num;
+	char **enviro;
+	char **new;
+	int i, num;
 
-    if (!tokens[1] || tokens[2])
-    {
-        fprintf(stderr,"%s: VARIABLE\n", name);
-        return (-1);
-    }
-    enviro = environ;
-    for (num = 0; *enviro; num++, enviro++)
-    ;
-    new = malloc((num + 1) * sizeof(char*));
-    if (new == NULL)
-    {
-        perror("Error in memory allocation");
-        exit(EXIT_FAILURE);
-    }
-    enviro = environ;
-    for (i = 0; i < num; i++, enviro++)
-    {
-        if (strncmp(*enviro, tokens[1], strlen(tokens[1])))
-            new[i] = *enviro;
-        else
-            i--;
-    }
-    new[i] = NULL;
-    environ = new;
-    free(new);
-    return (-1);
+	if (!tokens[1] || tokens[2])
+	{
+		fprintf(stderr, "%s: VARIABLE\n", name);
+		return (-1);
+	}
+	enviro = environ;
+	for (num = 0; *enviro; num++, enviro++)
+		;
+	new = malloc((num + 1) * sizeof(char *));
+	if (new == NULL)
+	{
+		perror("Error in memory allocation");
+		exit(EXIT_FAILURE);
+	}
+	enviro = environ;
+	for (i = 0; i < num; enviro++)
+	{
+		if (strncmp(*enviro, tokens[1], strlen(tokens[1])))
+		{
+			new[i] = *enviro;
+			i++;
+		}
+	}
+	new[i] = NULL;
+	environ = new;
+	free(new);
+	return (-1);
 }
 
 /**
  * setenv_fun - sets enviroment variable
- * @token: array of strings from stdin
+ * @tokens: array of strings from stdin
  * @name: name of the program
  * Return: -1 on success
-*/
+ */
 
 int setenv_fun(char **tokens, char *name)
 {
-    char **enviro;
-    char **new;
-    int i, num, len1, len2;
+	char **enviro;
+	char **new;
+	int i, num, len1, len2;
 
-    if (!tokens[1] || !tokens[2] || tokens[3])
-    {
-        fprintf(stderr,"%s: VARIABLE VALUE\n", name);
-        return (-1);
-    }
-    enviro = environ;
-    for (num = 0; *enviro; num++, enviro++)
-    ;
-    new = malloc((num + 2) * sizeof(char *));
-    if (new == NULL)
-    {
-        perror("Error in memory allocation");
-        exit(EXIT_FAILURE);
-    }
-    enviro = environ;
-    for (i = 0; i < num; i++, enviro++)
-    {
-        new[i] = *enviro;
-    }
-    len1 = strlen(tokens[1]);
-    len2 = strlen(tokens[2]);
-    new[i] = malloc(len1 + len2 + 2);
-    snprintf(new[i], len1 + len2 + 2, "%s=%s", tokens[1], tokens[2]);
-    new[++i] = NULL;
-    environ = new;
-    free(new);
-    return (-1);
+	if (!tokens[1] || !tokens[2] || tokens[3])
+	{
+		fprintf(stderr, "%s: VARIABLE VALUE\n", name);
+		return (-1);
+	}
+	enviro = environ;
+	for (num = 0; *enviro; num++, enviro++)
+		;
+	new = malloc((num + 2) * sizeof(char *));
+	if (new == NULL)
+	{
+		perror("Error in memory allocation");
+		exit(EXIT_FAILURE);
+	}
+	enviro = environ;
+	for (i = 0; i < num; i++, enviro++)
+	{
+		new[i] = *enviro;
+	}
+	len1 = strlen(tokens[1]);
+	len2 = strlen(tokens[2]);
+	new[i] = malloc(len1 + len2 + 2);
+	snprintf(new[i], (len1 + len2 + 2), "%s=%s", tokens[1], tokens[2]);
+	new[++i] = NULL;
+	environ = new;
+	free(new[--i]);
+	free(new);
+	return (-1);
 }
